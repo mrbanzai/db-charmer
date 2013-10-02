6e1c3512c8e92f8950d975ae62c89aa57ccdae70
Michael Porter <michael@skookum.com>
2013-10-02 12:48:03 -0400

    Fix Rake tasks path

--- lib/db_charmer/railtie.rb
+++ lib/db_charmer/railtie.rb
@@ -3,7 +3,7 @@ require 'rails/railtie'
 module DbCharmer
   class Railtie < Rails::Railtie
     rake_tasks do
-      load 'db_charmer/tasks/databases.rake'
+      load 'tasks/databases.rake'
     end
   end
 end
